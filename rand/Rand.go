b52fb915fe267ebec5c4b901b304500c40e241e9
Adam Presley <adam@adampresley.com>
2019-01-13 14:24:26 -0600

    Random string generator

---
+++ rand/Rand.go
@@ -0,0 +1,21 @@
+package rand
+
+import (
+	"math/rand"
+	"time"
+)
+
+/*
+String generates a random alpha-numeric string
+*/
+func String(length int) string {
+	characters := "abcdefghijklmnopqrstuvwxyz0123456789"
+	b := make([]byte, length)
+	seed := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	for index := range b {
+		b[index] = characters[seed.Intn(len(characters))]
+	}
+
+	return string(b)
+}
