41a87eb7f93a6e61bb85899c3a91a1368502bf27
Adam Presley <adam@adampresley.com>
2021-06-13 16:58:04 -0500

    Added some additional HTTP functions

--- restclient/IHTTPClient.go
+++ restclient/IHTTPClient.go
@@ -11,4 +11,17 @@ IHTTPClient is an interface over the Go HttpClient
 */
 type IHTTPClient interface {
 	Do(req *http.Request) (*http.Response, error)
+	SetTransport(transport *http.Transport)
+}
+
+type HTTPClient struct {
+	*http.Client
+}
+
+func (h *HTTPClient) Do(req *http.Request) (*http.Response, error) {
+	return h.Client.Do(req)
+}
+
+func (h *HTTPClient) SetTransport(transport *http.Transport) {
+	h.Client.Transport = transport
 }
--- restclient/MockHttpClient.go
+++ restclient/MockHttpClient.go
@@ -9,9 +9,14 @@ import (
 )
 
 type MockHttpClient struct {
-	DoFunc func(req *http.Request) (*http.Response, error)
+	DoFunc           func(req *http.Request) (*http.Response, error)
+	SetTransportFunc func(transport *http.Transport)
 }
 
 func (m *MockHttpClient) Do(req *http.Request) (*http.Response, error) {
 	return m.DoFunc(req)
+}
+
+func (m *MockHttpClient) SetTransport(transport *http.Transport) {
+	m.SetTransportFunc(transport)
 }
